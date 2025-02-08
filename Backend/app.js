1c53cf9b8fc0a30062b30863ba1dcfcc37f066f3
Shadyx6 <shadkhanxox@gmail.com>
2025-02-08 19:48:16 +0530

    backend bug fixes

--- Backend/app.js
+++ Backend/app.js
@@ -20,14 +20,6 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 
-const path = require("path");
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
-  });
-}
 
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
